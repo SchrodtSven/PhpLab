0f8d52171865d41a1acb25e755cb33a84093ca31
Sven Schrodt <sven@schrodt.club>
2026-02-03 13:45:53 +0100

    Another commit  -

--- gitme.sh
+++ gitme.sh
@@ -31,7 +31,8 @@ echo "</pre>" >> README.md
 
 # Adding, committing & pushing with message from STDIN
 git add .
-now = $(date)
+#now = $(date)
+now = "blaoup"
 echo $now
 git commit -m "$1 - $now"
 git push origin

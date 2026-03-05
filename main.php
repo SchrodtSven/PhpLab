03b94ca0af8b0ab32979c88f4fec454960ddc906
Sven Schrodt <sven@schrodt.club>
2026-03-05 15:59:52 +0100

    Cleaning bot ...

--- main.php
+++ main.php
@@ -1,5 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Testing stuff file 
+ * 
+ * @package PhpLab
+ * @author  Sven Schrodt <sven@schrodt.nrw>
+ * @version 0.1
+ * @since 2025-03-06
+ * @link https://github.com/SchrodtSven/PhpLab
+ */
+
+
 require_once 'src/PhpLab/Autoload.php';
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -13,13 +26,11 @@ use SchrodtSven\PhpLab\Wtf\Comm\HttpParser;
 //echo (new BaseClient())->search("Spider-Man");
 
 # $uri = "https://schrodt.nrw/bin/zce53.jpg";
-$uri = 'https://schrodt.nrw';
+$uri = 'https://schrodt.nrw/now.php'; // HTTP resource with minimal payload for testing purpose
 $hc = new CurlClient();
 $hc->get($uri);
-//var_dump($hc->getHeader());
+var_dump($hc);
 //var_dump($hc->getPayload());
-var_dump($hc->getHeaders());
-var_dump($hc->getHttpStatus());
-var_dump($hc->getVersion());
-
-
+// var_dump($hc->getHeaders());
+// var_dump($hc->getHttpStatus());
+// var_dump($hc->getVersion());
